7b3a004a9dd913724dbd97438e7aecc1768bc100
Mario Andres Villamizar Palacio <mario.villamizar@hotmail.com>
2017-01-09 12:42:15 -0500

    Initial Commit

---
+++ RappiTest-iOS-Cocoapods/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RappiTest-iOS-Cocoapods
+//
+//  Created by Mario Andres Villamizar Palacio on 1/9/17.
+//  Copyright © 2017 Mario Andres Villamizar Palacio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
